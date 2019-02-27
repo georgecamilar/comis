65c14fe122ba7c8a7ba513eaadc6503d27ee4173
george.camilar <georgecamilar2@gmail.com>
2019-02-27 15:58:17 +0200

    first-commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Comis Voiajor:
+    #road
+    It's not about the destination, it's the road that matters mr. Comis-Voiajor
+    AI
+    1. Greedy just choose cheapest road in each state
+    2. Backtracking for the win
---
+++ comisb.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class ComisVoiajor_back:
+    #Gotta put your back in it
+
+    def __init__(self, pointList):
+        self.checkpoints = pointList
+
+    def isSolution(self):
+        pass
+
+    def back(self):
+        pass
+
+    def isValid(self):
+        pass
+
+    def printCurrentBoardState(self):
+        print (checkpoints)
